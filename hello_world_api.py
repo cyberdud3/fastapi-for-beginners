03dc53316c482025891489087a14dc3ffd5b9abc
abhijith_lz <abhijith@lzindia.co.in>
2022-12-14 15:01:05 +0530

    get api - path parameters

--- hello_world_api.py
+++ hello_world_api.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+employees = {
+    1 : {
+        "name" : "Abhijith Sudhakar",
+        "designation": "DevOps Engineer",
+    }, 
+    2 : {
+        "name" : "Gayathri S V",
+        "designation": "Software Engineer"
+    }
+}
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/get-employee/{emp_id}")
+async def get_employee(emp_id: int):
+    return employees[emp_id]
